a5d34c372705fb03631704367e5682304cb56073
Shaharia Azam <shaharia.azam@gmail.com>
2016-07-24 08:57:22 -0600

    GPIO Pin command integrated

--- gpio.py
+++ gpio.py
@@ -1,12 +1,20 @@
 import falcon
 
 import json
-
+from gpiozero import LED, Button
+from time import sleep
 
 class GPIO(object):
 
     def on_get(self, req, resp, pin, in_out, value):
-        print pin, in_out, value
-        resp.body = json.dumps({'message': 'Hello world!'})
+	led = LED(int(pin))
+	if (in_out == 'write' and value == 'true'):
+		#print "write-true"
+		led.on()
+	if in_out == 'write' and value == 'false':
+		#print "write-false"
+		led.off()
+
+        resp.body = json.dumps({'pin': pin, 'in_out': in_out, 'value': value})
         resp.content_type = 'application/json'
         resp.status = falcon.HTTP_200
